0d359d61b7abc48072077525046665f29a6200cb
Joe Xie <ugb14129@gmail.com>
2021-10-05 22:48:47 -0500

    refactor

--- products.py
+++ products.py
@@ -1,18 +1,15 @@
 import os # operating system
 
-products = []
-if os.path.isfile('products.csv'):
-	print('Yes, Found it')
-	with open('products.csv', 'r', encoding = 'utf-8') as f:
+def read_file(filename):
+	products = []
+			
+	with open(filename, 'r', encoding = 'utf-8') as f:
 		for line in f:
 			if 'products 商品,price 价格' in line:
 				continue
 			name , price = line.strip().split(',')
 			products.append([name, price])
-	print(products)
-
-else:
-	print('Nah, cannot find it')	
+	return products	
 
 
 # read file
@@ -20,24 +17,44 @@ else:
 
 
 # Enter data
-while True:
-	name = input('Please enter product name: ')
-	if name == 'q':
-		break
-	price = input('Please enter product price: ')	
-	# p = []
-	# p.append(name)
-	# p.append(price)
-	# products.append(p)
-	products.append([name, price])
-print(products)		
+def user_input(products):
+	while True:
+		name = input('Please enter product name: ')
+		if name == 'q':
+			break
+		price = input('Please enter product price: ')	
+		# p = []
+		# p.append(name)
+		# p.append(price)
+		# products.append(p)
+		products.append([name, price])
+	print(products)	
+	return products
+
 
 # Print all data
-for p in products:
-	print(p[0], 'price is ', p[1])
+def print_products(products):
+	for p in products:
+		print(p[0], 'price is ', p[1])
+
 
 # Write Data
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-	f.write('products 商品,price 价格\n')
-	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n') 
+def write_file(filename, products):
+	with open(filename, 'w', encoding = 'utf-8') as f:
+		f.write('products 商品,price 价格\n')
+		for p in products:
+			f.write(p[0] + ',' + p[1] + '\n') 
+
+
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename):
+		print('Yes, Found it')
+		products = read_file(filename)
+	else:
+		print('Nah, cannot find it')
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)		
+
+main()	
